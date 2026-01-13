8ab9884fc7020c95c07bfe1df94d737f696ba4bd
waybackarchiver <66856220+waybackarchiver@users.noreply.github.com>
2026-01-13 01:49:38 +0000

    release: add version v0.4.8.21

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.15
 # Reset version number to zero everytime TOR_VERSION changes.
 export BRAVE_TOR_VERSION="2"
 
-export TOR_VERSION="0.4.8.17"
+export TOR_VERSION="0.4.8.21"
 
 export ZLIB_VERSION="1.3.1"
 export LIBEVENT_VERSION="2.1.12-stable"
@@ -15,6 +15,6 @@ export OPENSSL_VERSION="3.0.13"
 export ZLIB_HASH="9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
 export LIBEVENT_HASH=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
 export OPENSSL_HASH="88525753f79d3bec27d2fa7c66aa0b92b3aa9498dafd93d7cfa4b3780cdae313"
-export TOR_HASH="79b4725e1d4b887b9e68fd09b0d2243777d5ce3cd471e538583bcf6f9d8cdb56"
+export TOR_HASH="eaf6f5b73091b95576945eade98816ddff7cd005befe4d94718a6f766b840903"
 
 export DOCKER="$(command -v docker || command -v podman)"
